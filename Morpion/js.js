4c878e974e9036916535b8950f4e43dfaf34749d
Chelmid <lo.chelmi@gmail.com>
2017-11-13 14:02:35 +0100

    function reset en cours

--- Morpion/js.js
+++ Morpion/js.js
@@ -21,46 +21,56 @@ function verifier(){
 		if(true){
 			if(cases[0].innerHTML == joueur1 && cases[1].innerHTML == joueur1 && cases[2].innerHTML == joueur1){
 					console.log(cases[0].innerHTML,cases[1].innerHTML,cases[2].innerHTML)
-					alert(" joueur 1 win")
+					alert(" joueur 1 avec les symboles "+ joueur1 +" win")
+					reset()
 			}
 			if(cases[3].innerHTML == joueur1 && cases[4].innerHTML == joueur1 && cases[5].innerHTML == joueur1){
 					console.log(cases[3].innerHTML,cases[4].innerHTML,cases[5].innerHTML)
-					alert(" joueur 1 win")
+					alert(" joueur 1 avec les symboles "+ joueur1 +" win")
+					reset()
 			}
 			if(cases[6].innerHTML == joueur1 && cases[7].innerHTML == joueur1 && cases[8].innerHTML == joueur1){
 					console.log(cases[6].innerHTML,cases[7].innerHTML,cases[8].innerHTML)
-					alert(" joueur 1 win")
+					alert(" joueur 1 avec les symboles "+ joueur1 +" win")
+					reset()
 			}
 			if(cases[0].innerHTML == joueur1 && cases[4].innerHTML == joueur1 && cases[8].innerHTML == joueur1){
 					console.log(cases[0].innerHTML,cases[4].innerHTML,cases[8].innerHTML)
-					alert(" joueur 1 win")
+					alert(" joueur 1 avec les symboles "+ joueur1 +" win")
+					reset()
 			}
 			if(cases[2].innerHTML == joueur1 && cases[4].innerHTML == joueur1 && cases[6].innerHTML == joueur1){
 					console.log(cases[2].innerHTML,cases[4].innerHTML,cases[6].innerHTML)
-					alert(" joueur 1 win")
+					alert(" joueur 1 avec les symboles "+ joueur1 +" win")
+					cases.innerHTML = ''
 			}
 
 		// condition pour le O
 		if(true){
 			if(cases[0].innerHTML == joueur2 && cases[1].innerHTML == joueur2 && cases[2].innerHTML == joueur2){
 					console.log(cases[0].innerHTML,cases[1].innerHTML,cases[2].innerHTML)
-					alert(" joueur 2 win")
+					alert(" joueur 1 avec les symboles "+ joueur2 +" win")
+					reset()
 			}
 			if(cases[3].innerHTML == joueur2 && cases[4].innerHTML == joueur2 && cases[5].innerHTML == joueur2){
 					console.log(cases[3].innerHTML,cases[4].innerHTML,cases[5].innerHTML)
-					alert(" joueur 2 win")
+					alert(" joueur 1 avec les symboles "+ joueur2 +" win")
+					reset()
 			}
 			if(cases[6].innerHTML == joueur2 && cases[7].innerHTML == joueur2 && cases[8].innerHTML == joueur2){
 					console.log(cases[6].innerHTML,cases[7].innerHTML,cases[8].innerHTML)
-					alert(" joueur 2 win")
+					alert(" joueur 1 avec les symboles "+ joueur2 +" win")
+					reset()
 			}
 			if(cases[0].innerHTML == joueur2 && cases[4].innerHTML == joueur2 && cases[8].innerHTML == joueur2){
 					console.log(cases[0].innerHTML,cases[4].innerHTML,cases[8].innerHTML)
-					alert(" joueur 2 win")
+					alert(" joueur 1 avec les symboles "+ joueur2 +" win")
+					reset()
 			}
 			if(cases[2].innerHTML == joueur2 && cases[4].innerHTML == joueur2 && cases[6].innerHTML == joueur2){
 					console.log(cases[3].innerHTML,cases[4].innerHTML,cases[6].innerHTML)
-				alert(" joueur 2 win")
+				alert(" joueur 1 avec les symboles "+ joueur2 +" win")
+				reset()
 			}
 		}
 	}
@@ -70,7 +80,7 @@ function verifier(){
 cases.forEach(element => element.addEventListener("click", affichage));
 console.log(cases);
 
-// tour par tour 
+// tour par tour
 var t = 0;
 function tour(){
 	t++;
@@ -79,11 +89,15 @@ function tour(){
 
 // function pour l'affichage
 function affichage(){
-	if(this.innerHTML == false){
-		this.innerHTML = tour()
-	}
-	cases.innerHTML = verifier()
+	if(this.innerHTML = tour()){
+		cases.innerHTML = verifier()
+		reset()
 	/*if((cases.innerHTML = verifier()) == true){
 		cases.innerHTML = ""
-	}*/
+	}*/}
+}
+
+function reset(){
+			this.innerHTML = ''
+			console.log(cases)
 }
