4132f73387313c33334ea6b9f9a26bbae21fe283
Chelmid <lo.chelmi@gmail.com>
2017-12-11 18:11:47 +0100

    modifie et commencement du pile ou face php

---
+++ pile ou face php/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<form action="" method="">
+		<label for="pile">
+			<input type="radio" checked="checked" name="pile">Pile
+		</label>
+		<label for="face">
+			<input type="radio" name="face">Face
+		</label>
+
+		<?php 
+			$pile = rand(0,10);
+			$face = rand(0,10);
+
+			var_dump($pile)
+
+			function random(){
+				if(){
+
+				}
+			}
+
+			echo "C'est" 
+		?>
+
+	</form>
+</body>
+</html>
