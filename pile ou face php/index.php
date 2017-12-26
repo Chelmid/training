5795ebcbfc88ad498ba6db890091090334ad9899
Chelmid <lo.chelmi@gmail.com>
2017-12-26 20:57:42 +0100

    pile ou face 80% fini

--- pile ou face php/index.php
+++ pile ou face php/index.php
@@ -1,33 +1,52 @@
-<!DOCTYPE html>
-<html lang="fr">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
 </head>
 <body>
-	<form action="" method="">
-		<label for="pile">
-			<input type="radio" checked="checked" name="pile">Pile
-		</label>
-		<label for="face">
-			<input type="radio" name="face">Face
-		</label>
+	<form method="POST" action="index.php">
+    
+    <input type="radio" id="Choice1"  name="chance" value="Pile" checked="checked" >Pile
+    <label for="Choice1"></label>
 
-		<?php 
-			$pile = rand(0,10);
-			$face = rand(0,10);
+    <input type="radio" id="Choice2" name="chance" value="Face">Face
+    <label for="Choice2"></label>
+    
+    <br>
+    
+    	<?php
+			if(isset($_POST['lancer'])){
+				if(rand(1,2) > 1){
+					var_dump($_POST['chance']);
+					var_dump(mt_rand(1,2));
+					echo 'Pile';
+					$valeur = "Pile";
+				}else{
+					var_dump($_POST['chance']);
+					var_dump(mt_rand(1,2));
+					echo "Face";
+					$valeur = 'Face';
+				}
 
-			var_dump($pile)
-
-			function random(){
-				if(){
-
+				if($_POST['chance'] == $valeur){
+					echo 'vous avez gagné !';
+				}else{
+					echo'vous avez perdu !';
 				}
 			}
 
-			echo "C'est" 
-		?>
+			
 
+			
+			/*var_dump($_POST['chance']);
+			var_dump(rand(1,2));*/
+		?>
+	
+	<br>
+	
+	<input type="submit" name="lancer" value="valider">
+	
 	</form>
 </body>
 </html>
